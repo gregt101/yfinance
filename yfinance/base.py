a17f5d793acb63a60bacad14e1a8c79f23d045d8
gregt101 <gregt101@gmail.com>
2020-06-26 22:00:09 -0700

    Update base.py

--- yfinance/base.py
+++ yfinance/base.py
@@ -290,7 +290,7 @@ class TickerBase():
             response = urllib.request.urlopen(url,myHeaders)
             holders = _pd.read_html(response) 
         if len(holders)<=1:
-             _time.sleep(1)
+            _time.sleep(1)
             myHeaders = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36', 'Referer': 'https://www.google.com/', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
             response = urllib.request.urlopen(url,myHeaders)
             holders = _pd.read_html(response) 
