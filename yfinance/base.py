3602ec249253dae7ecdbbcef63b1452d1e229ec5
gregt101 <gregt101@gmail.com>
2020-09-07 20:22:21 -0700

    Update base.py

--- yfinance/base.py
+++ yfinance/base.py
@@ -301,8 +301,8 @@ class TickerBase():
         data = utils.get_json(url, proxy)
         print(data)
         # holders
-        """
         url = "{}/{}/holders".format(self._scrape_url, self.ticker)
+        """        
         holders = _pd.read_html(url)
         self._major_holders = holders[0]
         if len(holders) > 1: 
